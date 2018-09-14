8454da6e60d723349c8926b1c8085b31c3c69839
CaioSonoda <ra94045@uem.br>
2018-09-14 15:57:18 -0300

    Lognatural

--- calc.c
+++ calc.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <math.h>
+
 
 // FEATURE1
 
 
 
 // FEATURE2
+
+void log_natural(int x){
+x = 2.718282;
+logaritmo_natural = log(x);
+printf(“Logaritmo natural de x %.2f = %.2f \n”,x,logaritmo_natural);
+}
+
 
 
 // FEATURE3
